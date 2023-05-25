a912b0616d62815239c1cf1e45a231b779b1f2bf
Baier1g <134508184+Baier1g@users.noreply.github.com>
2023-05-25 12:52:30 +0200

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,58 @@
+import PySimpleGUI as sg
+import re
+from shunting_yard import calout
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8','9']
+operators = ['+', '-', '/', '*', '(', ')']
+
+def add_numbers(num1, num2):
+    return num1 * 10 + num2
+
+def CBtn(button_text):
+    return sg.Button(button_text, button_color=('white', 'blue'), size=(4, 1), font=("Helvetica", 20))
+
+def run_gui():
+    sg.theme('DarkAmber')   # Add a touch of color
+    # All the stuff inside your window.
+    layout = [  [sg.Text('Input numbers'), sg.Input()],
+                [CBtn(t) for t in ('1', '2', '3', '+', 'BKSP')],
+                [CBtn(t) for t in ('4', '5', '6', '-')],
+                [CBtn(t) for t in ('7', '8', '9', '*')],
+                [CBtn(t) for t in ('(','0',')', '/')],
+                [sg.Text('Set precision (decimalplaces)'), sg.Input()],
+                [sg.Button('Enter'), sg.Button('Cancel')]]
+
+    # Create the Window
+    window = sg.Window('Calculator', layout)
+    history = []
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+            break
+        
+        if event in numbers or event in operators:
+            if values[0] == None:
+                window[0].update(event)
+            else:    
+                window[0].update(f'{values[0]}{event}')
+
+        if event == 'BKSP':
+            my_string = values[0]
+            # Might be worth changing the slicing thing
+            new_string = my_string[:-1]
+            print(new_string)
+            window[0].update(new_string)
+        
+        if values[1] == "":
+            values[1] = 2
+        # If the value dict hadn't updated, line 22 could fix the problem
+        #list(minvariable.values())
+        if event == 'Enter':
+            res = calout(values[0],int(values[1]))
+            window[0].update(res)
+            history.append(res)
+    window.close()
+
+if __name__ == "__main__":
+    run_gui()
---
+++ shunting_yard.py
@@ -0,0 +1,234 @@
+import re
+from collections import namedtuple
+
+opinfo = namedtuple('Operator', 'precedence associativity')
+operator_info = {
+    "+": opinfo(0, "L"),
+    "-": opinfo(0, "L"),
+    "*": opinfo(1, "L"),
+    "/": opinfo(1, "L"),
+    "!": opinfo(2, "L"),
+    "^": opinfo(2, "R"),
+}
+
+def calculator(num1, num2, operator):
+    operations = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y,
+        '^': lambda x, y: x ** y
+    }
+    operation_func = operations.get(operator)
+    if operation_func:
+        result = operation_func(num1, num2)
+        return result
+    else:
+        print("Invalid operation")
+
+
+
+def tokenize(input_string):
+    cleaned = re.sub(r'\s+', "", input_string)
+    chars = list(cleaned)
+
+    output = []
+    state = ""
+    buf = ""
+
+    while len(chars) != 0:
+        char = chars.pop(0)
+
+        if char.isdigit():
+            if state != "num":
+                output.append(buf) if buf != "" else False
+                buf = ""
+
+            state = "num"
+            buf += char
+
+        elif char in operator_info.keys() or char in ["(", ")"]:
+            output.append(buf) if buf != "" else False
+            buf = ""
+
+            output.append(char)
+
+        else:
+            if state != "func":
+                output.append(buf) if buf != "" else False
+                buf = ""
+
+            state = "func"
+            buf += char
+
+    output.append(buf) if buf != "" else False
+    return output
+
+
+def shunt(tokens):
+    tokens += ['end']
+    operators = []
+    output = []
+
+    while len(tokens) != 1:
+        current_token = tokens.pop(0)
+
+        if current_token.isdigit():
+            # Is a number
+            #print("number", current_token)
+            output.append(current_token)
+
+        elif current_token in operator_info.keys():
+            # Is an operator
+            #print("op", current_token)
+            
+            while True:
+                if len(operators) == 0:
+                    break
+
+                satisfied = False
+
+                if operators[-1].isalpha():
+                    # is a function
+                    satisfied = True
+
+                if operators[-1] not in ["(", ")"]:
+                    if operator_info[operators[-1]].precedence > operator_info[current_token].precedence:
+                        # operator at top has greater precedence
+                        satisfied = True
+
+                    elif operator_info[operators[-1]].precedence == operator_info[current_token].precedence:
+                        if operator_info[operators[-1]].associativity == "left":
+                            # equal precedence and has left associativity
+                            satisfied = True
+
+                satisfied = satisfied and operators[-1] != "("
+
+                if not satisfied:
+                    break
+
+                output.append(operators.pop())
+
+            operators.append(current_token)
+
+        elif current_token == "(":
+            # Is left bracket
+            #print("left", current_token)
+            operators.append(current_token)
+
+        elif current_token == ")":
+            # Is right bracket
+            #print("right", current_token)
+
+            while True:
+                if len(operators) == 0:
+                    break
+
+                if operators[-1] == "(":
+                    break
+
+                output.append(operators.pop())
+
+            if len(operators) != 0 and operators[-1] == "(":
+                operators.pop()
+
+        else:
+            # Is a function name
+            #print("func", current_token)
+            operators.append(current_token)
+
+    output.extend(operators[::-1])
+
+    return output
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def build_parse_tree(postfix_expression):
+    stack = []
+    
+    for token in postfix_expression.split():
+        if token.isdigit():
+            node = Node(token)
+            stack.append(node)
+        else:
+            right_node = stack.pop()
+            left_node = stack.pop()
+            node = Node(token)
+            node.left = left_node
+            node.right = right_node
+            stack.append(node)
+    
+    return stack[0]
+
+
+def print_parse_tree(node, indent=''):
+    if node is None:
+        return
+    
+    print(indent + node.value)
+    
+    if node.left:
+        print_parse_tree(node.left, indent + '  |__')
+    
+    if node.right:
+        print_parse_tree(node.right, indent + '  |__')
+
+def evaluate_parse_tree(node):
+    if node is None:
+        return 
+    
+    left_operand = evaluate_parse_tree(node.left)
+    right_operand = evaluate_parse_tree(node.right)
+    
+    if left_operand is None or right_operand is None:
+        return int(node.value)
+    else:
+        return calculator(left_operand, right_operand, node.value)
+
+#tokens = tokenize("350 / ( 4 + 2 ) ^ 3")
+#postfix = " ".join(shunt(tokens))
+#parse = build_parse_tree(postfix)
+#print(postfix)
+#print_parse_tree(parse)
+#value = evaluate_parse_tree(parse)
+def testing():
+    tests = [
+    ("1+2", 3),
+    ("1+2*3", 7),
+    ("1+2 * 3 + 4", 11),
+    ("1 + 2 * 3 + 4 * 5", 27),
+    ("5 * ( 4 + 3 ) / 35", 1),
+    ("( 5 / 9 ) * 9 ^ 2", 45),
+    ("( 5 ^ 2 / 9 ) * 3", 8.33),
+    ("((5^3/9^2)^2)/10", 0.24)
+    ]
+    for enum, (test, expected_value) in enumerate(tests):
+        tokens = tokenize(test)
+        postfix = " ".join(shunt(tokens))
+        parse_tree = build_parse_tree(postfix)
+        omnom = print_parse_tree(parse_tree)
+        value = evaluate_parse_tree(parse_tree)
+        value = round(value,2)
+        print(value)
+        if (value != expected_value):
+            print(f"Test {enum} failed")
+            print(postfix)
+        else:
+            print("LESFUCKIGNGO")
+
+def calout(usrin, deci = 2):
+    tokens = tokenize(usrin)
+    postfix = " ".join(shunt(tokens))
+    parse_tree = build_parse_tree(postfix)
+    omnom = print_parse_tree(parse_tree)
+    value = evaluate_parse_tree(parse_tree)
+    value = round(value,deci)
+    return value
+    
+
+if __name__ == "__main__":
+  testing()
